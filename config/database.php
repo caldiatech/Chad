3736b4ae3afa935915b4c510472f319f5f1cd5f6
smit.h.lad <smit.h.lad@doyenhub.com>
2023-05-30 14:28:31 +0530

    config database changes

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' =>array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET SESSION SQL_BIG_SELECTS=1',
+            ),
+            'strict'    => false,
         ],
 
         'pgsql' => [
