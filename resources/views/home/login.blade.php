6e1c88320e04ead0b110d35963d7142b52f68e4f
sonal.l.khunt <sonal.l.khunt@doyenhub.com>
2024-09-13 15:33:15 +0530

    Registration and Contact page

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -38,49 +38,51 @@
                                     @if(Session::has('error'))
                                         <div class="uk-alert uk-alert-danger">{!!Session::get('error')!!}</div>
                                     @endif
-                                    <div class="social-link">
+                                    {{--<div class="social-link">
                                         <a href="#"><img src="{{ asset('_new_collection/assets/images/google.png') }}" alt="">Login with Google</a>
                                         <a href="#"><img src="{{ asset('_new_collection/assets/images/facebook.png') }}" alt="">Login with Facebook</a>
                                     </div>
                                     <div class="or-condition">
                                         <span>OR</span>
-                                    </div>                                   
-                                    {!! Form::open(array('url' => '/login', 'method' => 'post',  'class' => 'row-fluid account-login input-100')) !!}
-                                        <div class="form-field">
-                                            <img class="form-icon" src="{{ asset('_new_collection/assets/images/mail-line.png') }}" alt="">
-                                            <label class="float-lbl">Email *</label>
-                                            <input type="email" name="email" placeholder="abc@domain.com" required>
-                                        </div>
-                                        <div class="form-field">
-                                            <img class="form-icon" src="{{ asset('_new_collection/assets/images/lock.png') }}" alt="">
-                                            <label class="float-lbl">Password *</label>
-                                            <input type="password" name="password" placeholder="***********" required>
-                                            <!-- <a href="#" class="show-pass"><img src="{{ asset('_new_collection/assets/images/eye.png') }}" alt=""></a> -->
-                                        </div>
-                                        <div class="form-field form-field-flex">
-                                            <!-- <div class="check-group">
-                                                <label class="lbl-check">Remember me
-                                                    <input type="checkbox">
-                                                    <span class="checkmark"></span>
-                                                </label>
-                                            </div> -->
-                                            <div class="forgot-pass">
-                                                <a href="{{ url('/forgot-password')}}">Forgot Password?</a>
+                                    </div>--}}                                   
+                                    <div class="my-4">
+                                        {!! Form::open(array('url' => '/login', 'method' => 'post',  'class' => 'row-fluid account-login input-100')) !!}
+                                            <div class="form-field">
+                                                <img class="form-icon" src="{{ asset('_new_collection/assets/images/mail-line.png') }}" alt="">
+                                                <label class="float-lbl">Email *</label>
+                                                <input type="email" name="email" placeholder="abc@domain.com" required>
                                             </div>
-                                        </div>
-                                        <div class="form-field">
-                                            <button type="submit" name="login" class="theme-btn">LOGIN</button>
-                                        </div>
-                                        <p>
-                                            Don’t have an account? 
-                                            <a href="{{ url('/registration') }}">Register</a>
-                                        </p>
-                                        @if($cart_count > 0)
+                                            <div class="form-field">
+                                                <img class="form-icon" src="{{ asset('_new_collection/assets/images/lock.png') }}" alt="">
+                                                <label class="float-lbl">Password *</label>
+                                                <input type="password" name="password" placeholder="***********" required>
+                                                <!-- <a href="#" class="show-pass"><img src="{{ asset('_new_collection/assets/images/eye.png') }}" alt=""></a> -->
+                                            </div>
+                                            <div class="form-field form-field-flex">
+                                                <!-- <div class="check-group">
+                                                    <label class="lbl-check">Remember me
+                                                        <input type="checkbox">
+                                                        <span class="checkmark"></span>
+                                                    </label>
+                                                </div> -->
+                                                <div class="forgot-pass">
+                                                    <a href="{{ url('/forgot-password')}}">Forgot Password?</a>
+                                                </div>
+                                            </div>
+                                            <div class="form-field">
+                                                <button type="submit" name="login" class="theme-btn">LOGIN</button>
+                                            </div>
                                             <p>
-                                                {!! Html::link('guest-checkout', "Checkout as Guest",array('class'=>'uk-button uk-button-primary')) !!}
-                                            </p>  
-                                        @endif                                                                       
-                                    {!! Form::close() !!}
+                                                Don’t have an account? 
+                                                <a href="{{ url('/registration') }}">Register</a>
+                                            </p>
+                                            @if($cart_count > 0)
+                                                <p>
+                                                    {!! Html::link('guest-checkout', "Checkout as Guest",array('class'=>'uk-button uk-button-primary')) !!}
+                                                </p>  
+                                            @endif                                                                       
+                                        {!! Form::close() !!}
+                                    </div>
                                 </div>                                        
                             </div>
                         </div>
