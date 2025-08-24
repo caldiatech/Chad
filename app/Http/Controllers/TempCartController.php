f14fd65c5c3b0f1531c3461c882322712776cd98
smitlad <smit.h.lad@doyenhub.com>
2025-08-24 12:31:12 +0530

    commision percentage changes

--- app/Http/Controllers/TempCartController.php
+++ app/Http/Controllers/TempCartController.php
@@ -858,8 +858,8 @@ class TempCartController extends BaseController
 			$sales_manager_commission_total= $discount_formula * 0.1;
 		} else {
 			if( ( Session::get('couponSource') == 'Shop' )){
-				$sales_manager_commission_total = ($discount_formula- $total_graphik_cost) * 0.10;
-				$shop_owner_commission_total = ($discount_formula- $total_graphik_cost) * 0.40;
+				$sales_manager_commission_total = $discount_formula * 0.10;
+				$shop_owner_commission_total = $discount_formula * 0.40;
 				  Log::debug('---------shop_owner_commission 863--------------');
         Log::debug(message: '0.40');
 			} else {
@@ -883,7 +883,7 @@ class TempCartController extends BaseController
 		 Log::debug('---------couponSource 881--------------');
         Log::debug(Session::get('couponSource'));
 		if($temp_cart[0]['is_custom'] == 0 && ( Session::get('couponSource') != 'Shop' )){
-			$shop_owner_commission_total = ($discount_formula - $total_graphik_cost) * 0.50;
+			$shop_owner_commission_total = $discount_formula * 0.50;
 			  Log::debug('---------shop_owner_commission 885--------------');
         Log::debug('0.50');
 		}
