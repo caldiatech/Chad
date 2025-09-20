4696519666086500ddde8b8dd70616367a589b48
smitlad <smit.h.lad@doyenhub.com>
2025-09-20 17:46:53 +0530

    add totoal dd

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -82,8 +82,11 @@ class Cart extends Eloquent
 				}
 				//******END for product options***********//
 
-				$total =  ((int) $cartDisplays->quantity * (int) $cartDisplays->product_price);
-				dd($cartDisplays->quantity ,$cartDisplays->product_price);
+				//$total =  ((int) $cartDisplays->quantity * (int) $cartDisplays->product_price);
+				$price = "0.0";
+				$price = (float) str_replace(',', '', $cartDisplays->product_price); // 1075.00
+				$total = (int)$cartDisplays->quantity * $price; 
+				dd($total);
 				$total =  $total + $totalOptionPrice;
 
 				// $total =  ($cartDisplays->quantity * $cartDisplays->product_price)+$totalOptionPrice + $cartDisplays->fldCartShippingPrice;
